2193578e329b263bd690bef52140703d36b8e6d1
Francesc Esplugas <fesplugas@intraducibles.net>
2008-05-26 21:24:59 +0200

    Don't require mocha to test plugin.

--- test/markdown_fu_test.rb
+++ test/markdown_fu_test.rb
@@ -1,8 +1,7 @@
 require 'test/unit'
 require 'rubygems'
 require 'active_record'
-require 'mocha'
-require "#{File.dirname(__FILE__)}/../init"
+require 'markdown_fu'
 
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :dbfile => ":memory:")
 
@@ -44,25 +43,23 @@ class MarkdownFuTest < Test::Unit::TestCase
     teardown_db
   end
 
+  def test_translate
+    assert_equal "<p>Hello World!</p>", MarkdownFu.translate("Hello World!")
+  end
+
   def test_should_convert_all_markdown_fields_defined_in_the_class_and_super_classes
-    post = PostAndComment.new(:body=>'test body', :comment=>'test comment', :summary=>'test summary')
-    MarkdownFu.expects(:translate).with('test comment').returns("comment converted")
-    MarkdownFu.expects(:translate).with('test body').returns("body converted")
-    MarkdownFu.expects(:translate).with('test summary').returns("summary converted")
+    post = PostAndComment.new(:body => "test body", :comment => "test comment", :summary => "test summary")
     post.save
-    assert_equal "summary converted",  post.summary_html
-    assert_equal "comment converted",  post.generated_comment
-    assert_equal "body converted",  post.body_html
+    assert_equal "<p>test body</p>",  post.body_html
+    assert_equal "<p>test comment</p>",  post.generated_comment
+    assert_equal "<p>test summary</p>",  post.summary_html
   end
 
   def test_should_not_convert_blank_fields
-    post = PostAndComment.new(:body => '', :comment => nil)
-    MarkdownFu.expects(:translate).never
+    post = PostAndComment.new(:body => "", :comment => nil)
     post.save
-  end
-
-  def test_translate_should_be_working
-    assert_equal "<p>teste</p>", MarkdownFu.translate("teste")
+    assert_equal nil, post.body_html
+    assert_equal nil, post.generated_comment
   end
 
 end
